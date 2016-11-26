cf6b16ab51bdebed7e843762933a31811b278c91
Ryan Fuller <Ryan.Fuller@gradsingames.com>
2016-11-26 15:44:53 +0000

    comments

--- cordance/CordanceVeiwer.java
+++ cordance/CordanceVeiwer.java
@@ -14,13 +14,23 @@ public class CordanceVeiwer {
 	//the first element int he arraylist of the hashmap is the chapter in the book
 	private ArrayList<String> positionName;//holds the chapeters of the words
 	
-	// constructor sets up data
+	/**
+	 *  constructor sets up data
+	 * @param passedWordIndex
+	 * @param passedWordCataloge
+	 * @param passedPositionName
+	 */
 	public CordanceVeiwer(ArrayList<String> passedWordIndex, HashMap<String, ArrayList<Integer>> passedWordCataloge ,ArrayList<String> passedPositionName ) {
 		wordIndex = passedWordIndex;
 		wordCataloge = passedWordCataloge;
 		positionName = passedPositionName;
 	}
-	// returns a string with all the words cordances in with num number of words to the left and right
+	/**
+	 *  returns a string with all the words cordances in with num number of words to the left and right
+	 * @param word
+	 * @param num
+	 * @return
+	 */
 	public String getCordance(String word, int num){
 		ArrayList<Integer> temp = wordCataloge.get(word);
 		String cordance = "";
@@ -37,7 +47,11 @@ public class CordanceVeiwer {
 		}
 		return cordance;
 	}
-	// allows reseting of data
+	/**
+	 *  allows reseting of data
+	 * @param passedWordIndex
+	 * @param passedWordCataloge
+	 */
 	public void setData(ArrayList<String> passedWordIndex, HashMap<String, ArrayList<Integer>> passedWordCataloge ) {
 		wordIndex = passedWordIndex;
 		wordCataloge = passedWordCataloge;
