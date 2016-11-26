18a3ac7fd7aa4ea22d774031a3b78e7c2d6b271d
Ryan Fuller <Ryan.Fuller@gradsingames.com>
2016-11-26 13:18:05 +0000

    added codranceviewer class

---
+++ src/CordanceViewer.java
@@ -0,0 +1,17 @@
+
+public class CordanceViewer {
+
+	private String searchTerm; // is the word to generate a cordance of
+	
+	private int n; // this is the intager of words before and after
+	//the search term to be printed to the screen
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
