eb6f06ab9dd96355cbb5df95a8b0375cd9468929
Ryan Fuller <fullerryan42@gmail.com>
2016-12-12 11:07:20 +0000

    fixed minor things

--- bookView/DirectoryPusher.java
+++ bookView/DirectoryPusher.java
@@ -1,10 +1,7 @@
 package bookView;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
 
 import CordanceParser.ParsedCordance;
 
